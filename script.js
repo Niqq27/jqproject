e48dd6745b00e01e73a6f60734b2a632dccdb90a
Niklas Kaiser <kaiser_niklas@live.de>
2017-01-30 11:52:49 +0100

    second

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+$("body").prepend("<div id='container'> </div>");
+for (var i = 0; i <16; i++) {
+	for (var i = 0; i <16; i++) {
+	$("#container").prepend("<div class='butt'></div>");
+}
+}
+
+});
