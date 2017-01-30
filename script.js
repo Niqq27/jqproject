750ac258e975003eee2c9145cd86342f8caba4a2
Niklas Kaiser <kaiser_niklas@live.de>
2017-01-30 12:02:59 +0100

    thrid

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $("body").prepend("<div id='container'> </div>");
 for (var i = 0; i <16; i++) {
 	for (var i = 0; i <16; i++) {
 	$("#container").prepend("<div class='butt'></div>");
+	console.log("prepended");
 }
 }
 
