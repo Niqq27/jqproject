8562f429e1e935f2b3997b97cc974865e422f2db
Niklas Kaiser <kaiser_niklas@live.de>
2017-01-30 14:18:46 +0100

    kk

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 
-$("body").prepend("<div id='container'> </div>");
+generateGrid();
+colorSquares();
+
+});
+function generateGrid() {
+
+	$("body").prepend("<div id='container'> </div>");
 for (var i = 0; i <16; i++) {
 	for (var j = 0; j <16; j++) {
 	$("#container").prepend("<div class='butt'></div>");
@@ -9,4 +15,13 @@ for (var i = 0; i <16; i++) {
 }
 }
 
-});
+};
+
+function colorSquares(){
+
+	$(".butt").mouseenter(function(){
+		$(this).css("background-color", "blue");
+	})
+
+};
+
